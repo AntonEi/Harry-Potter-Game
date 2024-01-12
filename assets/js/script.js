721c8aa89d678c7144c0a71a004ea50c90c896d9
AntonEi <>
2024-01-12 15:45:08 +0000

    add css and js for 653px screen and higher.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,25 @@ var checkDead = setInterval(function () {
     var harryTop = parseInt(window.getComputedStyle(harry).getPropertyValue("top"));
     var dobbyLeft = parseInt(window.getComputedStyle(dobby).getPropertyValue("left"));
 
+    if (isTablet()) {
+        if (dobbyLeft < 45 && dobbyLeft > 0 && harryTop >= 190 && harryTop <= 250) {
+        youLose();
+        }
+    } else {
     
-    if (dobbyLeft < 45 && dobbyLeft > 0 && harryTop >= 50 && harryTop <= 150) {
-        dobby.style.animation = "none";
-        dobby.style.display = "none";
-        alert("You lose.");
-        clearInterval(checkDead); 
-    }
+        if (dobbyLeft < 45 && dobbyLeft > 0 && harryTop >= 50 && harryTop <= 150) {
+        youLose();
+        }
+    }    
 }, 10);
+
+function youLose() {
+    dobby.style.animation = "none";
+    dobby.style.display = "none";
+    alert("You lose.");
+    clearInterval(checkDead);
+}
+
+function isTablet() {
+    return window.matchMedia("(min-width: 653px)").matches;
+}
