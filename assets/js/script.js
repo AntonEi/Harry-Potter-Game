a8b8cc9b2477e785d17727212e97f5a317b354a8
AntonEi <>
2024-01-12 11:53:39 +0000

    Add checkdead function so you lose when dobby hits harry

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,16 @@ function jump(){
         harry.classList.remove("animate");
     },500);
 }
+
+var checkDead = setInterval(function () {
+    var harryTop = parseInt(window.getComputedStyle(harry).getPropertyValue("top"));
+    var dobbyLeft = parseInt(window.getComputedStyle(dobby).getPropertyValue("left"));
+
+    
+    if (dobbyLeft < 45 && dobbyLeft > 0 && harryTop >= 50 && harryTop <= 150) {
+        dobby.style.animation = "none";
+        dobby.style.display = "none";
+        alert("You lose.");
+        clearInterval(checkDead); 
+    }
+}, 10);
