485cc0275b2135c8610aff84f1ad8b823937a90d
AntonEi <>
2024-01-13 13:17:15 +0000

    HTML for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,7 @@ var checkDead = setInterval(function () {
 function youLose() {
     dobby.style.animation = "none";
     dobby.style.display = "none";
-    alert("You lose.");
+    startQuiz();
     clearInterval(checkDead);
 }
 
@@ -76,3 +76,7 @@ setTimeout(function(){
     startGame();
     clearInterval(timerInterval);
 }, 6000);
+
+function startQuiz(){
+    
+}
